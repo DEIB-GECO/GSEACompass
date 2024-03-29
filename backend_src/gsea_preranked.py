3e16ea2a7b1316c45fd0825fa3673f9809b730be
Antonino Ciancimino <antonino.ciancimino@mail.polimi.it>
2024-03-29 11:49:45 +0100

    Optimise chip conversion
    Chip conversion from one gene notation to another employed pandas.Dataframe.apply
    function and used to take long CPU usage, now uses the Pandas.Dataframe.join
    function and it's almost immediate.

--- backend_src/gsea_preranked.py
+++ backend_src/gsea_preranked.py
@@ -6,24 +6,16 @@ import gseapy as gp
 from gseapy import gseaplot
 from gseapy import gseaplot2
 
-from utils import gene_chip_conversion
-
 gene_sets_path = sys.argv[1]
 rnk_list_path = sys.argv[2]
 chip_path = sys.argv[3]
 
-rnk_list = pd.read_csv(rnk_list_path, header=None, sep="\t")
-chip = pd.read_csv(chip_path, header=0, sep="\t")
+rnk_list = pd.read_csv(rnk_list_path, header=None, index_col=0, sep="\t")
+chip = pd.read_csv(chip_path, header=0, index_col=0, sep="\t")
 
-# First and second column of ranked list as a DataFrames
-rnk_first_col = rnk_list.iloc[:, 0]
-rnk_second_col = rnk_list.iloc[:, 1]
- 
-orthologs = rnk_first_col.apply(lambda x : gene_chip_conversion(chip, x))
+rnk_orthologs = rnk_list.join(chip)[["Gene Symbol", 1]].reset_index(drop=True).dropna()
 
-rnk_list_orthologs = pd.concat([orthologs, rnk_second_col], axis=1).dropna()
-
-pre_res = gp.prerank(rnk=rnk_list_orthologs,
+pre_res = gp.prerank(rnk=rnk_orthologs,
                      gene_sets=gene_sets_path,
                      threads=8,
                      min_size=5,
--- backend_src/utils.py
+++ backend_src/utils.py
@@ -1,9 +1,3 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def gene_chip_conversion(chip, prob_set_id):
-    if chip[chip["Probe Set ID"] == prob_set_id]["Gene Symbol"].size >= 1:
-        return chip[chip["Probe Set ID"] == prob_set_id]["Gene Symbol"].values[0]
-    else:
-        return None
-    
